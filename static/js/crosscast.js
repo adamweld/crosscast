0693eadd2f85e6faa1a00cfb40e60f278fd3c8e2
Adam Weld <adam.weld@gmail.com>
2022-04-18 21:45:34 -0700

    js testing

--- static/js/crosscast.js
+++ static/js/crosscast.js
@@ -2,3 +2,14 @@ function myFunction(x) {
     let d = new Date();
     alert("Current datetime: " + d + "\nYou passed in: " + x);
 }
+
+function getWeatherUrl(lat, long, hour) {
+
+}
+
+function addimage(id) {
+    var img = new Image();
+    img.src = "https://www.google.com/images/srpr/logo4w.png"
+    img_home.appendChild(img);
+    document.getElementById(id).src = img.src
+}
