5fa928a346f5e458aba014f764790b45afb128a0
Adam Weld <adam.weld@gmail.com>
2022-04-18 21:59:00 -0700

    js testing

--- static/js/crosscast.js
+++ static/js/crosscast.js
@@ -4,6 +4,12 @@ function myFunction(x) {
 }
 
 function getWeatherUrl(lat, long, hour) {
-    var url = "https://forecast.weather.gov/meteograms/Plotter.php?lat=47.50421&lon=-122.00455&wfo=SEW&zcode=WAZ505&gset=20&gdiff=10&unit=0&tinfo=EY5&ahour=0&pcmd=111011111100000000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&nocache=1650340298"
+    var url = `https://forecast.weather.gov/meteograms/Plotter.php?
+    lat=${lat}
+    &lon=${lon}
+    &wfo=SEW&zcode=WAZ505&gset=20&gdiff=10&unit=0&tinfo=EY5
+    &ahour=${hour}
+    &pcmd=111011111100000000000000000000000000000000000000000000000
+    &lg=en&indu=1!1!1&dd=0&nocache=1650340298`
     return url
 }
